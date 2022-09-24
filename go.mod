013f15822257ce12ab43d6f2491c716b1da0115c
homingricky <homingricky@gmail.com>
2022-09-24 18:07:17 +0800

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/homingricky/demo-go-package
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,7 @@
+package demogopackage
+
+import "fmt"
+
+func firstPrint(message string) {
+	fmt.Printf("MESSAGE - %v", message)
+}
